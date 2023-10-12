9e047afda39898546911197bcb0b8155453a49ac
sohyundoh <lyny.doh@gmail.com>
2023-10-13 01:50:56 +0900

    init

---
+++ tcs/src/main/java/org/sopt/tcs/TcsApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsApplication.class, args);
+	}
+
+}
